a0518003832fca4f009e8ac9842c5cdd46dd8119
root <root@jptr.red>
2017-07-05 19:34:09 +0100

    Updated for ssh support.

--- bin/tagger.js
+++ bin/tagger.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
 "use strict";
+const exec = require("child_process").exec;
 
-require("../lib/index.js");
+exec("eval `ssh-agent -s`; ssh-add; ssh-add -l", (err, stdout, stderr) => {
+	if(err) {
+		console.error(err);
+	} else {
+		console.log(stdout);
+	        require("../lib/index.js");
+	}
+});
